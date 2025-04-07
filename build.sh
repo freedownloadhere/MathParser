222a87367cd95df513316e35bb5ac825fd303653
freedownloadhere <freedownloadhere13@gmail.com>
2025-04-07 23:37:34 +0300

    we are using cmake now :(

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-if g++ -g -o mathparser.out src/Main.cpp src/Lexer.cpp src/Parser.cpp src/Token.cpp src/Expression.cpp src/Exception.cpp; then
-    echo 'built math parser'
-else
-    echo 'failed build xd'
-fi
